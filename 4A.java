5215ecb97b03df04381ec4616f6a3c46cc9ed7bf
ANUJ YADAV <52133014+ANUJ581@users.noreply.github.com>
2020-10-02 19:56:39 +0530

    updated scanner

--- 4A.java
+++ 4A.java
@@ -7,8 +7,8 @@ public class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	 Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+	 Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         if(n> 2 && n % 2 == 0)
 			System.out.println("YES");
 		else
